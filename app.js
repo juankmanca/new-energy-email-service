2a63f22ff7ba98792de8faaeccbb6ef9526b999f
juankmanca <juankmanca@gmail.com>
2023-01-23 09:13:40 -0500

    try fix CORS.1

--- app.js
+++ app.js
@@ -21,11 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+//https://new-energy-raltfb52j-juankmanca.vercel.app
 app.use(cors({
   origin: '*',
   methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['xAuth', 'Content-Type', 'X-Requested-With'],
+  preflightContinue: false,
   exposedHeaders: ['xAuth', 'Content-Type', 'Accept'],
 }));
 
