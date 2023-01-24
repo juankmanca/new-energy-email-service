4e38e0d0bfd3697d45fa49aec364ec47b45d973e
juankmanca <juankmanca@gmail.com>
2023-01-24 11:09:36 -0500

    try fix CORS.24

--- api/mail.router.js
+++ api/mail.router.js
@@ -5,7 +5,24 @@ const {
   main
 } = require('../controllers/mail.controller');
 
+const allowCors = fn => async (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
 /* POST send email. */
-router.post('/solicitarInformacion', main);
+router.post('/solicitarInformacion', allowCors(main), main);
 
 module.exports = router;
