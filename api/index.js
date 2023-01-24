fdaa020e3f82cf961451ae8af3623cb468b4989d
juankmanca <juankmanca@gmail.com>
2023-01-24 12:23:57 -0500

    try fix CORS.29

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const nodemailer = require("nodemailer");
+const fs = require('fs');
+const path = require("path");
 
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
@@ -18,9 +21,71 @@ const allowCors = fn => async (req, res) => {
   return await fn(req, res)
 }
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
+// async..await is not allowed in global scope, must use a wrapper
+const handler = async (req, res)  => {
+  try {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    // let testAccount = await nodemailer.createTestAccount();
+    // const email = req.body.email || null;
+    const email = 'juan@gmail.com'
+    if (!email) throw "Ingrese un correo!"
+
+    guardarCorreoEnArchivo(email)
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_SMTP,
+      port: 465,
+      // service: "gmail",
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    transporter.verify().then(async () => {
+      console.log('Ready for send emails');
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: `"Camilo Ocampo" <${process.env.MAIL_USER}>`, // sender address
+        to: process.env.MAIL_USER, // list of receivers
+        subject: "Solicitud de información.", // Subject line
+        html: "<p>El Cliente con el correo: <b>" + email + "</b> esta solicitando mas información<p>", // plain text body
+        // html: "<b>Hello world?</b>", // html body
+      }).catch((err) => {
+        throw err;
+      })
+
+      console.log("Message sent: %s", info.messageId);
+      res.status(200).send("Melo");
+
+    }).catch((err) => {
+      throw err;
+    })
+
+  } catch (error) {
+    console.log('Error >>:', error);
+    res.status(500).send(error);
+  }
+}
+
+function guardarCorreoEnArchivo(email) {
+  try {
+    pathFile = path.join(__dirname, '../public/logs/log.txt')
+    fs.appendFile(
+      pathFile,
+      email + '\n',
+      (err) => {
+        if (err) {
+          console.log('err >>:', err);
+        }
+      }
+    )
+  } catch (error) {
+    console.log('error >>:', error);
+  }
 }
 
 /* GET home page. */
