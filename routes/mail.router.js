f4f0b1b1c64d14898482b9ed4cba7f9278330caa
juankmanca <juankmanca@gmail.com>
2023-01-24 09:07:16 -0500

    try fix CORS.15

--- routes/mail.router.js
+++ routes/mail.router.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const allowCors = require('../api/handler')
 const {
   main
 } = require('../controllers/mail.controller');
+
+allowCors(main)
 
 /* POST send email. */
 router.post('/solicitarInformacion', main);
