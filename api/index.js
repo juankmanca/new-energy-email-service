e813b1982fa77e8193b0feae9f2b296d477bb313
juankmanca <juankmanca@gmail.com>
2023-01-25 14:29:56 -0500

    try fix CORS.41

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.send("La pagina de Inicio");
+  res.json({
+    message: 'La pagina de inicio'
+  });
 });
 
 module.exports = router;
